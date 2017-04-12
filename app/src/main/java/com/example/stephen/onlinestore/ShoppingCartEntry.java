d690a4d6e446e495eb81baf165434d21a28af4fc
SCaldwell1 <c13330621@mydit.ie>
2017-04-12 11:13:46 +0100

    cart entry

--- app/src/main/java/com/example/stephen/onlinestore/ShoppingCartEntry.java
+++ app/src/main/java/com/example/stephen/onlinestore/ShoppingCartEntry.java
@@ -8,6 +8,8 @@ class ShoppingCartEntry {
     private Product mProduct;
     private int mQuantity;
     public ShoppingCartEntry(Product p, int q) {
+        mProduct = p;
+        mQuantity = q;
     }
 
     public void setQuantity(int productQuantity) {
@@ -16,5 +18,9 @@ class ShoppingCartEntry {
 
     public int getQuantity() {
         return mQuantity;
+    }
+
+    public Product getProduct() {
+        return mProduct;
     }
 }
