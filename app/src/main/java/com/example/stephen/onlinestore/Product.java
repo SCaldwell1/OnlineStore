300025fc72516d455170bcdcb8009db3fc4eb2ea
SCaldwell1 <c13330621@mydit.ie>
2017-04-12 10:15:02 +0100

    product constructor

--- app/src/main/java/com/example/stephen/onlinestore/Product.java
+++ app/src/main/java/com/example/stephen/onlinestore/Product.java
@@ -1,8 +1,21 @@
 package com.example.stephen.onlinestore;
 
-/**
- * Created by Stephen on 12/04/2017.
- */
+import android.graphics.drawable.Drawable;
+
+
 
 class Product {
+    public String title;
+    public Drawable productImage;
+    public String description;
+    public double price;
+    public boolean selected;
+
+    public Product(String title, Drawable productImage, String description,
+                   double price) {
+        this.title = title;
+        this.productImage = productImage;
+        this.description = description;
+        this.price = price;
+    }
 }
